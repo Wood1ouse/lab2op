faf86ec454fa180d008caeb024fef824afcf99ab
Wood1ouse <m.mamontov-2024@kpi.ua>
2020-03-10 17:12:32 +0200

    output to console is managed

--- lab2.1.py
+++ lab2.1.py
@@ -1,7 +1,9 @@
 import os
+
 dir = os.path.relpath('resources')
 new_file = open('sum.csv', 'w')
 files = ('1.csv', '2.csv')
+
 for csv in files:
     with open('{0}/{1}'.format(dir, csv)) as file:
         num_line = 1
@@ -10,3 +12,43 @@ for csv in files:
                 num_line += 1
                 continue
             new_file.write(line)
+new_file.close()
+
+
+points = dict()
+countries = 0
+with open('sum.csv') as summer:
+    for line in summer:
+        array = line.split(',')
+        country = array[0]
+        points[country] = 0
+        countries += 1
+
+
+for i in range(1, countries + 1):
+    votes = []
+    with open('sum.csv') as summer:
+        for line in summer:
+            array = line.split(',')
+            votes.append([array[i], array[0]])
+        votes.sort(reverse=True)
+        prize = 12
+        for place in range(10):
+            if place < 2:
+                points[votes[place][1]] += prize
+                prize -= 2
+            else:
+                points[votes[place][1]] += prize
+                prize -= 1
+
+array_of_points = []
+for k, v in points.items():
+    array_of_points.append([v, k])
+
+array_of_points.sort(reverse=True)
+adequate_view = []
+for i in range(len(array_of_points)):
+    adequate_view.append([array_of_points[i][1], array_of_points[i][0]])
+
+for country, points in adequate_view:
+    print(country, ':', points)
